50473dbcef70b3c07edb6cf7bc1d9cfa1049f564
yvesivan <qyizborganio@tip.edu.ph>
2024-05-19 09:48:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_data(experimental_allow_widgets=True)
 def load_model():
-  model=tf.keras.models.load_model('model.h5')
+  model=tf.keras.models.load_model('Weather_Classification-Model.h5')
   return model
 model=load_model()
 st.write("""
